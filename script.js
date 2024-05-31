739d0ecf43967a6435f16e0d2a468b6f506b8f24
razmazlih <retraz123@gmail.com>
2024-05-31 22:24:56 +0300

    add login() function

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
 const url = "http://localhost:3000/"
 
-function getUsers() {
-
+// return all users
+async function getUsers() {
+    let allUsers;
+    await fetch(url + "users").then(res => {
+        if (!res.ok) {
+            throw new Error("Internet Problem")
+        }
+        return res.json()
+    })
+    .then(data => {
+        allUsers = data
+    })
+    .catch(error => {
+        alert("Server Error: " + error)
+    })
+    return allUsers
 }
 
-function login() {
+async function login() {
+    const allUsers = await getUsers()
+    const userInput = document.getElementById("usernameInputLogin").value;
+    const passwordInput = document.getElementById("passwordInputLogin").value;
+    for (let i = 0; i < allUsers.length; i++) {
+        const user = allUsers[i];
+        if (userInput == user["username"] || userInput == user["email"]) {
+            if (passwordInput == user["password"]) {
+                let keyOfUser = {
+                    username: user["username"],
+                }
+                localStorage.setItem("userKey", JSON.stringify(keyOfUser))
+                window.location.href = "./blog.html";
+                break
+            } else {
+                alert("Wrong Password!")
+                return
+            }
+        }
+    }
+    alert("User Not Found!")
 
 }
 
