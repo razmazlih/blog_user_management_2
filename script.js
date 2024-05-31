a48a4ff5090c7971d6b9e591ac87792e8e93b8b4
razmazlih <retraz123@gmail.com>
2024-05-31 14:43:48 +0300

    add script.js with empty functions

---
+++ script.js
@@ -0,0 +1,31 @@
+function getUsers() {
+    
+}
+
+function login() {
+    
+}
+
+function signUp() {
+    
+}
+
+function addPost() {
+    
+}
+
+function addComment() {
+    
+}
+
+function editPost() {
+    
+}
+
+function editComment() {
+    
+}
+
+async function displayPosts() {
+    
+}
