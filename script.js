dae29731e4a22444dd6e3e73f7c2e4ddfdbacbb1
razmazlih <retraz123@gmail.com>
2024-06-01 19:19:57 +0300

    add signUp() function

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ async function getUsers() {
         }
         return res.json()
     })
-    .then(data => {
-        allUsers = data
-    })
-    .catch(error => {
-        alert("Server Error: " + error)
-    })
+        .then(data => {
+            allUsers = data
+        })
+        .catch(error => {
+            alert("Server Error: " + error)
+        })
     return allUsers
 }
 
@@ -27,11 +27,11 @@ async function login() {
         if (userInput == user["username"] || userInput == user["email"]) {
             if (passwordInput == user["password"]) {
                 let keyOfUser = {
-                    username: user["username"],
+                    user_id: user["id"],
                 }
                 localStorage.setItem("userKey", JSON.stringify(keyOfUser))
                 window.location.href = "./blog.html";
-                break
+                return
             } else {
                 alert("Wrong Password!")
                 return
@@ -42,8 +42,61 @@ async function login() {
 
 }
 
-function signUp() {
+async function signUp() {
+    const allUsers = await getUsers();
+    const emailInput = document.getElementById("emailInputSignIn").value;
+    const usernameInput = document.getElementById("usernameInputSignIn").value;
+    const passwordInput = document.getElementById("passwordInputSignIn").value;
+    const passAgainInput = document.getElementById("passwordInputSignInVari").value;
 
+    for (let i = 0; i < allUsers.length; i++) {
+        const user = allUsers[i];
+        const validEmail = user["email"] === emailInput;
+        const validUsername = user["username"] === usernameInput;
+        if (validEmail) {
+            alert("Email taken. Please choose another.");
+            return;
+        }
+        if (validUsername) {
+            alert("Username taken. Please choose another.");
+            return;
+        }
+    }
+
+    if (passwordInput !== passAgainInput) {
+        alert("Passwords do not match. Please try again.");
+        return;
+    }
+
+    const newUser = {
+        username: usernameInput,
+        email: emailInput,
+        password: passwordInput
+    };
+
+    let myData;
+
+    await fetch(url + "users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newUser)
+    })
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Internet Problem");
+            }
+            return res.json();
+        })
+        .then(data => {
+            myData = data;
+        })
+        .catch(error => {
+            console.log("Error: " + error);
+        });
+    window.location.href = "./index.html";
+    alert("Success")
 }
 
 function addPost() {
@@ -55,7 +108,7 @@ function addComment() {
 }
 
 function editPost() {
-    
+
 }
 
 function editComment() {
