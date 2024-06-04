09e6e99afb9d898c1940a000d52599a49c268ac9
razmazlih <retraz123@gmail.com>
2024-06-04 08:08:46 +0300

    update server files

--- server.js
+++ server.js
@@ -2,31 +2,8 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const cors = require('cors');
 
-// Configure CORS
-const corsOptions = {
-    origin: 'https://razmazlih.github.io/', // Change this to your front-end origin
-    credentials: true, // Enable cookies and other credentials
-};
-
-server.use(cors(corsOptions));
 server.use(middlewares);
-server.use(jsonServer.bodyParser);
-
-server.use((req, res, next) => {
-    if (req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE') {
-        const authHeader = req.headers.authorization;
-        if (!authHeader) {
-            res.sendStatus(401);
-        } else {
-            next();
-        }
-    } else {
-        next();
-    }
-});
-
 server.use(router);
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
